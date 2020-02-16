686c793eae7efcf43e6e8a73945bdf4c2f176c70
Aitmambetov <m.aytmambetov@mail.ru>
2020-02-16 23:54:18 +0600

    2.11

---
+++ task11.py
@@ -0,0 +1,10 @@
+"""
+Given a string consisting of words separated by spaces. Determine how many
+words it has. To solve the problem, use the method count.
+"""
+word = str(input())
+
+number = int(word.count(" "))
+print(number + 1)
+
+
